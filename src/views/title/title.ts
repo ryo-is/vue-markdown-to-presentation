c39b1bb700a2e1edb08545b2583898f807418199
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-20 10:19:37 +0900

    fix:タイトルと自己紹介のスタイル修正

--- src/views/title/title.ts
+++ src/views/title/title.ts
@@ -3,6 +3,10 @@ import { transitionPageInfoType } from "@/types"
 
 @Component({})
 export default class Title extends Vue {
+  public title: string = `AWS-CDK for TypeScript
+  を紹介するぜ`
+  public subTitle: string = `kansai.ts#1
+  Ryosuke Izumi( is_ryo )`
   public transitionPageInfo: transitionPageInfoType = {
     preb: "/sectionTitle/end",
     next: "/selfIntroduction"
